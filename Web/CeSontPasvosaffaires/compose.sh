59a39531ade4b9665374f3bcece3fa5233982dae
Ne0re0 <neo.leborgne@gmail.com>
2023-12-16 18:17:24 +0100

    Ajout docker-compose.yml

--- Web/CeSontPasvosaffaires/compose.sh
+++ Web/CeSontPasvosaffaires/compose.sh
@@ -2,5 +2,5 @@
 
 sudo docker rm cesontpasvosaffaires01 --force
 sudo docker build -t cesontpasvosaffaires .
-sudo docker run -dp 8080:80 --network bridge --name cesontpasvosaffaires01 cesontpasvosaffaires
+sudo docker run -dp 8083:80 --network bridge --name cesontpasvosaffaires01 cesontpasvosaffaires
 
