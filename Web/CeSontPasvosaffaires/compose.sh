512025e4beeaa13f001ddab6861541ee509be907
Ne0re0 <neo.leborgne@gmail.com>
2023-12-20 11:45:58 +0100

    beautify

--- Web/CeSontPasvosaffaires/compose.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker rm cesontpasvosaffaires01 --force
-sudo docker build -t cesontpasvosaffaires .
-sudo docker run -dp 8083:80 --network bridge --name cesontpasvosaffaires01 cesontpasvosaffaires
-
